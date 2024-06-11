891dd96e34dc75b0ddb4138638352b029561649a
Sa021di <bekmuradsaydaxmetov@gmail.com>
2024-06-11 21:49:55 +0500

    Added-New-HomePage

--- jdu-portfolio-client/src/components/Pages/Recruitor/homePage/index.jsx
+++ jdu-portfolio-client/src/components/Pages/Recruitor/homePage/index.jsx
@@ -15,7 +15,7 @@ import { TopStudentsGet } from '../../../../services/student'
 import { Loginout } from '../../../../services/auth'
 import { useNavigate } from 'react-router-dom'
 import { ImageUpload } from '../../../../utils/imageUpload'
-
+import { Link } from 'react-router-dom';
 export default function HomePage({ user }) {
 
     const [JDU, setJDU] = useState({})
@@ -123,205 +123,123 @@ export default function HomePage({ user }) {
 
     return (
         <>
-            <div className={cls.HomePage} >
-                <div className={cls.HomePage__container} >
-                    <h2 className={cls.HomePage__title}>JDUの学生については</h2>
+            <div className={cls.HomePage}>
+                <div className={cls.HomePage__container}>
+                    <h1 className={cls.HomePage__titleh1}>Japan Digital University</h1>
+                    <hr className={cls.HomePage__line} />
+                    <h2 className={cls.HomePage__title}>
+                        <span className={cls.HomePage__titleBlack}>より良い</span>
+                        <br />
+                        <span className={cls.HomePage__titleRed}>明日へ</span>
+                    </h2>
                     <p className={cls.HomePage__text}>
-                        本プラットフォームでは、学生たちの日本語のスキルを簡単に確認でき、その他の素晴らしい特徴も垣間見ることができます。学生たちの日本語のスキルは一目でわかります。彼らの自己紹介やプロフィールには、日本語での経験やライティングスキルが詳細に記載されています。学生たちは様々なバックグラウンドを持っています。留学経験や日本語の資格を持つ者から、日本企業への就職を目指す者まで、多岐にわたる才能がここに集結しています。これからの時代を担うリーダーとなる学生たち。その中でも、日本語スキルを武器にビジョンを持ち、挑戦に果敢に立ち向かう彼らがいます。
+                        2020年に日本資本がウズベキスタンに設立し運営している正式な私立大学です。ウズベキスタンにあるサテライトキャンパスをJDUと呼びます。ウズベキスタンの学生は、提携している日本の大学の授業にオンラインで参加し、日本の大学の試験を経て単位取得、卒業を目指します。（日本とウズベキスタン両国の学位を取得し卒業することが可能です）卒業時には日本企業への就職を目指し、勉学に励む学生がたくさん入学しています。
                     </p>
-                    <div className={cls.HomePage__img}>
-                        {/* <img
-                            src={'/Image/image1.png'}
-                            alt='img'
-                            width={269}
-                            height={197}
-                        /> */}
-                    </div>
-
-                </div>
-
-                <div className={cls.HomePage__card}>
-                    <div className={cls.HomePage__card__card}>
-                        <h2 className={cls.HomePage__card__card__title}>{data2?.First?.count}</h2>
-                        {/* <p className={cls.HomePage__card__card__text}>Percent: {Math.floor(+data2?.First?.percentage)}%</p> */}
-                        <p className={cls.HomePage__card__card_role}>1年生</p>
-                    </div>
-                    <div className={cls.HomePage__card__card}>
-                        <h2 className={cls.HomePage__card__card__title}>{data2?.Second?.count}</h2>
-                        {/* <p className={cls.HomePage__card__card__text}>Percent: {Math.floor(+data2?.Second?.percentage)}%</p> */}
-                        <p className={cls.HomePage__card__card_role}>2年生</p>
-                    </div>
-                    <div className={cls.HomePage__card__card}>
-                        <h2 className={cls.HomePage__card__card__title}>{data2?.Third?.count}</h2>
-                        {/* <p className={cls.HomePage__card__card__text}>Percent: {Math.floor(+data2?.Third?.percentage)}%</p> */}
-                        <p className={cls.HomePage__card__card_role}>3年生</p>
-                    </div>
-                    <div className={cls.HomePage__card__card}>
-                        <h2 className={cls.HomePage__card__card__title}>{data2?.Fourth?.count}</h2>
-                        {/* <p className={cls.HomePage__card__card__text}>Percent: {Math.floor(+data2?.Fourth?.percentage)}%</p> */}
-                        <p className={cls.HomePage__card__card_role}>4年生</p>
-                    </div>
-                </div>
-
-                <div className={cls.HomePage__chart}>
-                    <div className={cls.HomePage__chart__wrap}>
-                        <h3 className={cls.HomePage__chart__title}>JLPT</h3>
-                        <p className={cls.HomePage__chart__text}>Japanese-Language Proficiency Test</p>
-                        <div className={cls.HomePage__test__wrap} >
-                            <div className={cls.HomePage__test__width}>
-                                <div className={cls.HomePage__test} style={{ borderBottom: `${Math.round((((JLPT?.N1 / maxValue) * 100) / 100) * 185) || 2}px solid #5627DC` }}>
-                                    {JLPT?.N1}
-                                </div>
-                                <p className={cls.HomePage__test_test} >N1</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={cls.HomePage__test} style={{ borderBottom: `${Math.round((((JLPT?.N2 / maxValue) * 100) / 100) * 185) || 2}px solid #5627DC` }}>
-                                    {JLPT?.N2}
-                                </div>
-                                <p className={cls.HomePage__test_test}>N2</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={cls.HomePage__test} style={{ borderBottom: `${Math.round((((JLPT?.N3 / maxValue) * 100) / 100) * 185) || 2}px solid #5627DC` }}>
-                                    {JLPT?.N3}
-                                </div>
-                                <p className={cls.HomePage__test_test} >N3</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={cls.HomePage__test} style={{ borderBottom: `${Math.round((((JLPT?.N4 / maxValue) * 100) / 100) * 185) || 2}px solid #5627DC` }}>
-                                    {JLPT?.N4}
-                                </div>
-                                <p className={cls.HomePage__test_test} >N4</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={cls.HomePage__test} style={{ borderBottom: `${Math.round((((JLPT?.N5 / maxValue) * 100) / 100) * 185) || 2}px solid #5627DC` }}>
-                                    {JLPT?.N5}
-                                </div>
-                                <p className={cls.HomePage__test_test}>N5</p>
-                            </div>
+                    <div className={cls.HomePage__images}>
+                        <div className={cls.HomePage__img}>
+                            <img
+                                src={'/Image/BigPicture.jpg'}
+                                alt='img'
+                                width={500}
+                                height={340}
+                            />
+                        </div>
+                        <div className={cls.HomePage__imgSmall}>
+                            <img
+                                src={'/Image/SmallPicture.jpg'}
+                                alt='img'
+                                width={355}
+                                height={213}
+                            />
                         </div>
                     </div>
-                    <div className={cls.HomePage__chart__wrap}>
-                        <h3 className={cls.HomePage__chart__title}>JDU日本語認定</h3>
-                        <p className={cls.HomePage__chart__text}>Japan Digital University</p>
-                        <div className={cls.HomePage__test__wrap}>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={`${cls.HomePage__test} ${cls.HomePage__test2}`} style={{ borderBottom: `${Math.round((((JDU?.Q1 / maxValue) * 100) / 100) * 185) || 2}px solid #DC7E27` }}>
-                                    {JDU?.Q1}
-                                </div>
-                                <p className={cls.HomePage__test2_test}>N1</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={`${cls.HomePage__test} ${cls.HomePage__test2}`} style={{ borderBottom: `${Math.round((((JDU?.Q2 / maxValue) * 100) / 100) * 185) || 2}px solid #DC7E27` }}>
-                                    {JDU?.Q2}
-                                </div>
-                                <p className={cls.HomePage__test2_test}>N2</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={`${cls.HomePage__test} ${cls.HomePage__test2}`} style={{ borderBottom: `${Math.round((((JDU?.Q3 / maxValue) * 100) / 100) * 185) || 2}px solid #DC7E27` }}>
-                                    {JDU?.Q3}
-                                </div>
-                                <p className={cls.HomePage__test2_test}>N3</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={`${cls.HomePage__test} ${cls.HomePage__test2}`} style={{ borderBottom: `${Math.round((((JDU?.Q4 / maxValue) * 100) / 100) * 185) || 2}px solid #DC7E27` }}>
-                                    {JDU?.Q4}
-                                </div>
-                                <p className={cls.HomePage__test2_test}>N4</p>
-                            </div>
-                            <div className={cls.HomePage__test__width}>
-                                <div className={`${cls.HomePage__test} ${cls.HomePage__test2}`} style={{ borderBottom: `${Math.round((((JDU?.Q5 / maxValue) * 100) / 100) * 185) || 2}px solid #DC7E27` }}>
-                                    {JDU?.Q5}
-                                </div>
-                                <p className={cls.HomePage__test2_test}>N5</p>
-                            </div>
-                        </div>
-                    </div>
+                    <Link to="/recruitor/students" className={cls.HomePage__button}>次へ ➜</Link>
                 </div>
             </div>
 
             {openMadal && !user?.isActive &&
-            <div className={cls.overlay} style={{ display: openMadal ? 'block' : 'none' }}>
-                <AddMadal
-                    role={"登録"}
-                    update={true}
-                    style={{ maxWidth: "775px" }}
-                    OnSubmit={handleSubmit(UpdateStudentFunc)}
-                    closeMadal={async () => {
-                        await Loginout()
-                        router('/auth/login')
-                    }}
-                >
-                    <AvatarInput
-                        onChange={(e) => hendleimg(e)}
-                        url={avatar || watchedFiles?.avatar}
-                        style={{ marginBottom: '43px' }}
-                    />
-                    <div className={cls.HomePage__addInputs}>
+                <div className={cls.overlay} style={{ display: openMadal ? 'block' : 'none' }}>
+                    <AddMadal
+                        role={"登録"}
+                        update={true}
+                        style={{ maxWidth: "775px" }}
+                        OnSubmit={handleSubmit(UpdateStudentFunc)}
+                        closeMadal={async () => {
+                            await Loginout()
+                            router('/auth/login')
+                        }}
+                    >
+                        <AvatarInput
+                            onChange={(e) => hendleimg(e)}
+                            url={avatar || watchedFiles?.avatar}
+                            style={{ marginBottom: '43px' }}
+                        />
+                        <div className={cls.HomePage__addInputs}>
 
-                        <AddInput
-                            register={{ ...register('firstName', { required: "名前は必要です！" }) }}
-                            type={"text"}
-                            label={"名字"}
-                            placeholder={"名字"}
-                            value={watchedFiles?.firstName || ''}
-                            alert={errors.firstName?.message}
-                            onChange={() => clearErrors("firstName")}
-                            style={{ marginBottom: "20px" }}
+                            <AddInput
+                                register={{ ...register('firstName', { required: "名前は必要です！" }) }}
+                                type={"text"}
+                                label={"名字"}
+                                placeholder={"名字"}
+                                value={watchedFiles?.firstName || ''}
+                                alert={errors.firstName?.message}
+                                onChange={() => clearErrors("firstName")}
+                                style={{ marginBottom: "20px" }}
 
-                        />
-                        <AddInput
-                            register={{ ...register('lastName', { required: "名字は必要です！" }) }}
-                            type={"text"}
-                            label={"名字"}
-                            placeholder={"名字"}
-                            value={watchedFiles?.lastName || ''}
-                            alert={errors.lastName?.message}
-                            onChange={() => clearErrors("lastName")}
-                            style={{ marginBottom: "20px" }}
+                            />
+                            <AddInput
+                                register={{ ...register('lastName', { required: "名字は必要です！" }) }}
+                                type={"text"}
+                                label={"名字"}
+                                placeholder={"名字"}
+                                value={watchedFiles?.lastName || ''}
+                                alert={errors.lastName?.message}
+                                onChange={() => clearErrors("lastName")}
+                                style={{ marginBottom: "20px" }}
 
-                        />
-                        <AddInput
-                            type={"text"}
-                            label={"Id"}
-                            placeholder={"Id"}
-                            value={user?.loginId}
-                            style={{ marginBottom: "20px" }}
-                            disabled={true}
-                        />
-                        <AddInput
-                            register={{ ...register('phoneNumber', { required: "電話番号は必要です！" }) }}
-                            type={"text"}
-                            label={"電話番号"}
-                            placeholder={"電話番号"}
-                            value={watchedFiles?.phoneNumber || ''}
-                            alert={errors.phoneNumber?.message}
-                            onChange={() => clearErrors("phoneNumber")}
-                            style={{ marginBottom: "20px" }}
-                        />
-                        <AddInput
-                            register={{ ...register('companyName', { required: "会社名は必要です！" }) }}
-                            type={"text"}
-                            label={"会社名"}
-                            placeholder={"会社名"}
-                            value={watchedFiles?.companyName || ''}
-                            alert={errors.companyName?.message}
-                            onChange={() => clearErrors("companyName")}
-                            style={{ marginBottom: "20px" }}
+                            />
+                            <AddInput
+                                type={"text"}
+                                label={"Id"}
+                                placeholder={"Id"}
+                                value={user?.loginId}
+                                style={{ marginBottom: "20px" }}
+                                disabled={true}
+                            />
+                            <AddInput
+                                register={{ ...register('phoneNumber', { required: "電話番号は必要です！" }) }}
+                                type={"text"}
+                                label={"電話番号"}
+                                placeholder={"電話番号"}
+                                value={watchedFiles?.phoneNumber || ''}
+                                alert={errors.phoneNumber?.message}
+                                onChange={() => clearErrors("phoneNumber")}
+                                style={{ marginBottom: "20px" }}
+                            />
+                            <AddInput
+                                register={{ ...register('companyName', { required: "会社名は必要です！" }) }}
+                                type={"text"}
+                                label={"会社名"}
+                                placeholder={"会社名"}
+                                value={watchedFiles?.companyName || ''}
+                                alert={errors.companyName?.message}
+                                onChange={() => clearErrors("companyName")}
+                                style={{ marginBottom: "20px" }}
 
-                        />
+                            />
 
-                        <AddInput
-                            type={"text"}
-                            label={"電子メール"}
-                            placeholder={"電子メール"}
-                            value={user?.email}
-                            alert={errors.email?.message}
-                            onChange={() => clearErrors("email")}
-                            style={{ marginBottom: "20px" }}
-                            disabled={true}
-                        />
-                    </div>
-                </AddMadal>
+                            <AddInput
+                                type={"text"}
+                                label={"電子メール"}
+                                placeholder={"電子メール"}
+                                value={user?.email}
+                                alert={errors.email?.message}
+                                onChange={() => clearErrors("email")}
+                                style={{ marginBottom: "20px" }}
+                                disabled={true}
+                            />
+                        </div>
+                    </AddMadal>
                 </div>
             }
             <Toaster />
