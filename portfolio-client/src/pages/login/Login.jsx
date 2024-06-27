06fd3501a436e6e4ef66144488cdf9cc2c9d59fe
jdu9B0007 <123530527+jdu9B0007@users.noreply.github.com>
2024-06-28 03:01:16 +0500

    removed not used lib from file

--- portfolio-client/src/pages/login/Login.jsx
+++ portfolio-client/src/pages/login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import styles from './Login.module.css';
 import logo from '../../assets/logo.png';
 import universityImage from '../../assets/university.png';
