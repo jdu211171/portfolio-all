5eafabd1a92b31b70e9505978a44767779278986
jdu9B0007 <123530527+jdu9B0007@users.noreply.github.com>
2024-06-29 01:39:59 +0500

    added table component, made to work with props

--- portfolio-client/src/pages/student/Student.jsx
+++ portfolio-client/src/pages/student/Student.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React from "react";
+
+import { Box } from "@mui/material";
+
+import Table from "../../components/table/Table";
 
 const Home = () => {
+  const headers = [
+    {
+      id: "name",
+      numeric: false,
+      disablePadding: true,
+      label: "学生",
+      type: "avatar",
+    },
+    {
+      id: "grade",
+      numeric: true,
+      disablePadding: false,
+      label: "学年",
+      type: "status",
+    },
+    {
+      id: "faculty",
+      numeric: true,
+      disablePadding: false,
+      label: "学部",
+    },
+    {
+      id: "japanese",
+      numeric: true,
+      disablePadding: false,
+      label: "日本語能力試験",
+    },
+    {
+      id: "action",
+      numeric: true,
+      disablePadding: false,
+      label: "アクション",
+    },
+  ];
+
+  const props = {
+    headers: headers,
+    data: "/api/students",
+  };
+
   return (
     <div>
       <h1>Student Page</h1>
-      <p>Welcome to the Student page!</p>
+      <Box sx={{ width: "100%" }}>
+        <>here should be filter</>
+      </Box>
+      <Table tableProps={props} />
     </div>
   );
 };
