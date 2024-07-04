15f496c10d26b54ce8fb0de62079252bb167e32a
jdu9B0007 <123530527+jdu9B0007@users.noreply.github.com>
2024-07-05 01:12:37 +0500

    deleted student detaild

--- portfolio-client/src/routes.jsx
+++ portfolio-client/src/routes.jsx
@@ -19,7 +19,6 @@ import Setting from "./pages/setting/Setting";
 import Student from "./pages/student/Student";
 import Recruiter from "./pages/recruiter/Recruiter";
 import Staff from "./pages/staff/Staff";
-import StudentDetail from "./pages/student/StudentDetail"; // Импортируем компонент страницы студента
 
 
 const AppRoutes = () => {
@@ -32,7 +31,6 @@ const AppRoutes = () => {
             <Route path="/student" element={<Student />} />
             <Route path="/recruiter" element={<Recruiter />} />
             <Route path="/staff" element={<Staff />} />
-            <Route path="/student/:id" element={<StudentDetail />} />
             <Route path="/settings" element={<Setting />} />
             <Route path="/help" element={<FAQ />} />
             <Route path="/profile" element={<Profile />}>
