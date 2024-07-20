12b1b749cb427117088a2779062cc6b9740cccf7
jdu9B0007 <123530527+jdu9B0007@users.noreply.github.com>
2024-07-20 22:48:23 +0500

    image zoom-pinch component added, styled

--- portfolio-client/src/components/Deliverables/Deliverables.jsx
+++ portfolio-client/src/components/Deliverables/Deliverables.jsx
@@ -17,10 +17,35 @@ const Deliverables = ({ title, data, editMode, updateEditData, keyName }) => {
     const { zoomIn, zoomOut, resetTransform } = useControls();
     return (
       <div className={styles.zoomContainer}>
-        <button className={styles.zoomButtonR} onClick={() => zoomIn()}>+</button>
-        <button className={styles.zoomButtonL} onClick={() => zoomOut()}>-</button>
+        <button className={styles.zoomButtonR} onClick={() => zoomIn()}>
+          +
+        </button>
+        <button className={styles.resetButton} onClick={() => resetTransform()}>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 4V1L8 5l4 4V6c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8h-2c0 5.5 4.5 10 10 10s10-4.5 10-10-4.5-10-10-10z"
+              fill="white"
+            />
+          </svg>
+        </button>
+        <button className={styles.zoomButtonL} onClick={() => zoomOut()}>
+          -
+        </button>
       </div>
     );
+  };
+
+  const props = {
+    width: 400,
+    height: 250,
+    zoomWidth: 500,
+    img: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   };
 
   return (
@@ -60,18 +85,17 @@ const Deliverables = ({ title, data, editMode, updateEditData, keyName }) => {
           </tr>
         </tbody>
       </table>
-      <TransformWrapper>
-        <TransformComponent>
-          <img
-            src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="test"
-            width="100%"
-          />
-        </TransformComponent>
-        <Controls />
-      </TransformWrapper>
-      <Box>
-        <img src="sample" alt="" />
+      <Box className={styles.imageBox}>
+        <TransformWrapper>
+          <TransformComponent>
+            <img
+              src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              alt="test"
+              width="100%"
+            />
+          </TransformComponent>
+          <Controls />
+        </TransformWrapper>
       </Box>
       <div>{data[0].codeLink}</div>
       <div>{data[0].imageLink}</div>
