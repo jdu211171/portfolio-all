fc6e7489722b819785ebda752a6057d5e4a19f34
jdu200027 <200027a@jdu.uz>
2024-11-26 17:36:45 +0500

    "dasdasdas"

--- portfolio-server/src/app.js
+++ portfolio-server/src/app.js
@@ -35,7 +35,7 @@ configureRoutes(app);
 
 
 app.get('/test', (req, res)=>{
-  res.status(200).json({message: "AMIME"});
+  res.status(200).json({message: "Lalaku"});
 });
 
 cron.schedule('0 4 * * *', async () => {
