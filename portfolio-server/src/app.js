a321ee776ba723698d3b6a24aaf702ace8272873
jdu200027 <200027a@jdu.uz>
2024-12-01 16:21:05 +0500

    "test cron 2"

--- portfolio-server/src/app.js
+++ portfolio-server/src/app.js
@@ -110,7 +110,7 @@ cron.schedule('0 4 * * *', async () => {
 
 // Test endpoint
 app.get('/test', (req, res) => {
-  res.status(200).json({ message: "aji buju" });
+  res.status(200).json({ message: "anime" });
 });
 
 // Serve the React app
