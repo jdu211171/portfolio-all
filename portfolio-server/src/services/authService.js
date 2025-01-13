38643a8aa30f420b36ff1e5d4d9ab92cfc42a88f
jdu9B0007 <botirovs03@gmail.com>
2025-01-13 11:50:41 +0500

    added aws cognito login

--- portfolio-server/src/services/authService.js
+++ portfolio-server/src/services/authService.js
@@ -1,41 +1,90 @@
-const bcrypt = require('bcrypt');
+const { CognitoIdentityProviderClient, InitiateAuthCommand, SignUpCommand, AdminConfirmSignUpCommand } = require('@aws-sdk/client-cognito-identity-provider');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const { Admin, Staff, Recruiter, Student } = require('../models');
+
+const cognitoClient = new CognitoIdentityProviderClient({ region: process.env.AWS_REGION });
 
 class AuthService {
   static async login(email, password, res) {
-    const userTypes = [Admin, Staff, Recruiter, Student];
+    const secretHash = crypto.createHmac('sha256', process.env.COGNITO_CLIENT_SECRET)
+      .update(email + process.env.COGNITO_CLIENT_ID)
+      .digest('base64');
 
-    for (const UserType of userTypes) {
-      const user = await UserType.findOne({ where: { email } });
-      if (user) {
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (isMatch) {
-          const token = jwt.sign(
-            { id: user.id, userType: UserType.name },
-            process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRATION }
-          );
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+        SECRET_HASH: secretHash,
+      },
+    };
+    try {
+      const command = new InitiateAuthCommand(params);
+      const response = await cognitoClient.send(command);
+      console.log(response)
+      const idToken = response.AuthenticationResult.IdToken;
+      const decodedToken = jwt.decode(idToken);
 
-          // Set the JWT token and userType as cookies
-          res.cookie('token', token, {
-            httpOnly: false,
-            secure: process.env.NODE_ENV === 'production',
-            expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000), // Convert expiresIn to milliseconds
-          });
+      const userType = decodedToken['custom:userType'];
+      const userId = decodedToken['sub'];
+      console.log("userType", userType)
+      console.log("userId", userId)
+      // Set the JWT token and userType as cookies
+      res.cookie('token', idToken, {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000),
+      });
 
-          // Also set userType as a separate cookie
-          res.cookie('userType', UserType.name, {
-            httpOnly: false, // Can be accessed on the client-side
-            secure: process.env.NODE_ENV === 'production',
-            expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000), // Convert expiresIn to milliseconds
-          });
-          return { userType: UserType.name, userData: { id: user.id, name: user.first_name + " " + user.last_name, studentId: user.student_id, photo: user.photo } };
-        }
-      }
+      res.cookie('userType', userType, {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000),
+      });
+
+      return { userType, userData: { id: userId, email } };
+    } catch (error) {
+      console.error("Error during login:", error);
+      throw new Error('Invalid credentials');
     }
+  }
 
-    throw new Error('Invalid credentials');
+  static async signUp(email, password, userType, additionalAttributes = {}) {
+    const secretHash = crypto.createHmac('sha256', process.env.COGNITO_CLIENT_SECRET)
+      .update(email + process.env.COGNITO_CLIENT_ID)
+      .digest('base64');
+
+    const params = {
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: email,
+      Password: password,
+      SecretHash: secretHash,
+      UserAttributes: [
+        { Name: 'email', Value: email },
+        { Name: 'custom:userType', Value: userType },
+        ...Object.keys(additionalAttributes).map(key => ({ Name: key, Value: additionalAttributes[key] })),
+      ],
+    };
+
+    try {
+      const command = new SignUpCommand(params);
+      await cognitoClient.send(command);
+
+      // Automatically confirm the user (optional, depending on your use case)
+      const confirmParams = {
+        UserPoolId: process.env.COGNITO_USER_POOL_ID,
+        Username: email,
+      };
+      const confirmCommand = new AdminConfirmSignUpCommand(confirmParams);
+      await cognitoClient.send(confirmCommand);
+
+      return { message: 'User registered successfully' };
+    } catch (error) {
+      console.error("Error during sign up:", error);
+      throw new Error('Error registering user: ' + error.message);
+    }
   }
 
   static async logout(res) {
